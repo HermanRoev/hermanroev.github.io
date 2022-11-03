2047395468033343f5644359b3a77f0db96a983f
HermanRoev <herman.rov@gmail.com>
2022-11-03 15:14:22 +0100

    Nav hides on scroll

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
-//Javascript fil, gjør at man kan gjennbruke funksjoner uten at man må kopiere de inn på alle sidene
 
+//Sets the width of the div containing the navigation menu to 0 or 250px
 function openNav() {
     document.getElementById("popout").style.width = "250px";
   }
 
 function closeNav() {
     document.getElementById("popout").style.width = "0px";
+}
+
+//Function to hide nav menu on scroll
+
+var scrollPosition = window.scrollY;
+window.onscroll = function() {
+  var newScrollPosition = window.scrollY;
+  if (scrollPosition > newScrollPosition) {
+    document.getElementById("navbar").style.top = "0";
+  }
+  else {
+    document.getElementById("navbar").style.top = "-85px"
+  }
+  scrollPosition = newScrollPosition;
 }
