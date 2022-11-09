d76cc38c028e9e3cb91c7033574ce91a8c2e4a0b
HermanRoev <herman.rov@gmail.com>
2022-11-09 14:45:42 +0100

    Startet med onscroll, page layout

--- js/animations.js
+++ js/animations.js
@@ -1,13 +1,6 @@
 //Scroll animation js for topic page :)
-window.onscroll = function() {    //Zoomer inn på et objekt før det gjemmes
+window.onscroll = function() {
     var position = window.scrollY;
-    document.getElementById("container").style.top = position;
-    var scale = position/20;
-    document.getElementById("check").style.scale = (1, scale);
-    if (position >= 200) { //Gjemmer når man scroller til et satt punkt
-      document.getElementById("container").style.visibility = "hidden";
-    }
-    else {
-      document.getElementById("container").style.visibility = "visible";
-    }
+    document.getElementById("box").style.top = position + "px";
+    document.getElementById("txt").style.left = position*7 + "px";
  }
