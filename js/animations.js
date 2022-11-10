14e678f320bf7ee8e68f97c01138ada36441c240
HermanRoev <herman.rov@gmail.com>
2022-11-10 14:07:44 +0100

    Uptodate

--- js/animations.js
+++ js/animations.js
@@ -2,11 +2,24 @@
 window.onscroll = function() {
     var position = window.scrollY;
     //document.getElementById("box").style.top = position + "px";
-    document.getElementById("txt").style.left = position*2 + "px";
     if (position > 500) {
         document.getElementById("txt").style.display = "none";
     }
     else {
         document.getElementById("txt").style.display = "flex";
+        document.getElementById("txt").style.left = position*1.5 + "px";
+    }
+
+    vrzoom();
+ }
+ 
+ function vrzoom() {
+    var zoom = document.getElementById("zoom");
+    var offset = zoom.offsetTop;
+    if (window.scrollY >= offset) {
+        document.getElementById("zoom").classList.add = "sticky";
+    }
+    else {
+        document.getElementById("zoom").classList.remove = "sticky";
     }
  }
