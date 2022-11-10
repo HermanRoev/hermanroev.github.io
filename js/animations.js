9d013a40374623e47986089f4273bc888706234d
HermanRoev <herman.rov@gmail.com>
2022-11-10 15:11:24 +0100

    Almost sticky

--- js/animations.js
+++ js/animations.js
@@ -1,6 +1,7 @@
 //Scroll animation js for topic page :)
 window.onscroll = function() {
     var position = window.scrollY;
+    console.log(position);
     //document.getElementById("box").style.top = position + "px";
     if (position > 500) {
         document.getElementById("txt").style.display = "none";
@@ -9,17 +10,29 @@ window.onscroll = function() {
         document.getElementById("txt").style.display = "flex";
         document.getElementById("txt").style.left = position*1.5 + "px";
     }
-
+    
     vrzoom();
+
  }
  
+ function getCoords(element) {
+    var ele = document.getElementById(element);
+    var top = 0;
+    while (ele.tagName != "BODY") {
+        top += ele.offsetTop;
+        ele = ele.offsetParent;
+    }
+    return top;
+ }
+
  function vrzoom() {
-    var zoom = document.getElementById("zoom");
-    var offset = zoom.offsetTop;
-    if (window.scrollY >= offset) {
-        document.getElementById("zoom").classList.add = "sticky";
+    var cords = getCoords("zoom");
+    console.log(cords);
+    var zoom = document.getElementById("zoom")
+    if (window.scrollY >= cords) {
+        zoom.classList.add("sticky");
     }
     else {
-        document.getElementById("zoom").classList.remove = "sticky";
+        zoom.classList.remove("sticky");
     }
  }
