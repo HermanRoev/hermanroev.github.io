ccf67e0e0c0a842d559713e54bbedf866fe3e0b5
HermanRoev <herman.rov@gmail.com>
2022-11-10 12:30:45 +0100

    Updates

--- js/animations.js
+++ js/animations.js
@@ -1,6 +1,12 @@
 //Scroll animation js for topic page :)
 window.onscroll = function() {
     var position = window.scrollY;
-    document.getElementById("box").style.top = position + "px";
-    document.getElementById("txt").style.left = position*7 + "px";
+    //document.getElementById("box").style.top = position + "px";
+    document.getElementById("txt").style.left = position*2 + "px";
+    if (position > 500) {
+        document.getElementById("txt").style.display = "none";
+    }
+    else {
+        document.getElementById("txt").style.display = "flex";
+    }
  }
