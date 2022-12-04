50a14ee3cc035954e447d71ed117f5238013866f
HermanRoev <herman.rov@gmail.com>
2022-12-04 16:34:36 +0100

    More comments

--- js/script.js
+++ js/script.js
@@ -1,20 +1,21 @@
 
-//Sets the width of the div containing the navigation menu to 0 or 250px
+// Sets the width of the div containing the navigation menu to 0 or 250px
+// Changes visibility to hide links, so they are not targetable while not on screen
 function openNav() {
     document.getElementById("popout").style.width = "250px";
-    document.getElementById("popout").style.visibility = "visible";
+    document.getElementById("popout").style.visibility = "visible"; 
   }
 
 function closeNav() {
     document.getElementById("popout").style.width = "0px";
-    document.getElementById("popout").style.visibility = "hidden";
+    document.getElementById("popout").style.visibility = "hidden";  
 }
 
-//Function to hide nav menu on scroll
+// Function to hide nav menu on scroll
 var scrollPosition = window.scrollY;
-window.onscroll = function() {  /* On Scroll event listener that calls a funtion each time a scroll event occurs */
-  var newScrollPosition = window.scrollY; /* Sets where you are on the page as the position */
-  if (scrollPosition > newScrollPosition) { /* Checks if you scrolled up or down and changes the position of the nav element based on that */
+window.onscroll = function() {  // On Scroll event listener that calls a funtion each time a scroll event occurs
+  var newScrollPosition = window.scrollY; // Sets where you are on the page as the position
+  if (scrollPosition > newScrollPosition) { // Checks if you scrolled up or down and changes the position of the nav element based on that
     document.getElementById("navbar").style.top = "0";
   }
   else {
